0693d5263e4f04f2e3260a6ca083e81fcddc65f4
jorgesoares2997 <jorgesoares2997@gmail.com>
2023-03-22 15:11:37 -0300

    Rotation

--- quinto.dart
+++ quinto.dart
@@ -1,4 +1,14 @@
+import 'dart:io';
+
 void main() {
-  String palavra = 'jazz';
-  print(palavra.hashCode);
+  print('qual palavra deseja inverter?');
+  var palavra = stdin.readLineSync()!;
+
+  var palavraInvertida = inversor(palavra);
+
+  print("${palavraInvertida}");
+}
+
+String inversor(texto) {
+  return texto.split('').reversed.join();
 }
